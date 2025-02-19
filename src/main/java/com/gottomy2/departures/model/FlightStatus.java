2ddc8341ec324084b47f3b898f3c8f74d0da834b
gottomy2 <igottomy@gmail.com>
2025-02-19 18:25:12 +0100

    update: fixed project structure to fit backend and frontend projects within one repository, fixed inital data from csv file to match enum types.

--- src/main/java/com/gottomy2/departures/model/FlightStatus.java
+++
@@ -1,8 +0,0 @@
-package com.gottomy2.departures.model;
-
-public enum FlightStatus {
-    PLANOWANY,
-    ODPRAWA,
-    OPÓŹNIONY,
-    ODWOŁANY
-}
--- src/main/java/com/gottomy2/departures/model/FlightZone.java
+++
@@ -1,6 +0,0 @@
-package com.gottomy2.departures.model;
-
-public enum FlightZone {
-    SCHENGEN,
-    NON_SCHENGEN
-}
